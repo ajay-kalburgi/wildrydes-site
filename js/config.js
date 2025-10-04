9814c028ef87c284ebd621cb17f56275fcdf635b
ajay-kalburgi <85450796+ajay-kalburgi@users.noreply.github.com>
2025-10-04 10:40:54 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_BFKPvRRaG', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6kgo1chbfrdupt1mcecaqmivdn', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_Lkffiwknt', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'uvpis7n3kadqbtinahicevk67', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
